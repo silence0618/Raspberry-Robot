483ab05794c385a311fae5b7a145ace07f337e33
silence0618 <fanglinxun@163.com>
2017-05-08 03:55:57 +0000

    accurate ultrasonic

--- ultrasonic.py
+++ ultrasonic.py
@@ -1,35 +1,32 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode(GPIO.BCM)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setwarnings(False)
 
-TRIG = 18
-ECHO = 12
+TRIG = 29
+ECHO = 22
 
-print "Distance Measurement In Progress"
+#print "Distance Measurement In Progress"
 
 GPIO.setup(TRIG,GPIO.OUT)
 GPIO.setup(ECHO,GPIO.IN)
 
-GPIO.output(TRIG, False)
-print "Waiting For Sensor To Settle"
-time.sleep(3)
+def dis():
+    GPIO.output(TRIG, False)
+    time.sleep(0.010)
+    GPIO.output(TRIG, True)
+    time.sleep(0.000011)
+    GPIO.output(TRIG,False)
+    while GPIO.input(ECHO)==0:
+        pulse_start = time.time()
+    while GPIO.input(ECHO)==1:
+        pulse_end = time.time()
+    pulse_duration = pulse_end - pulse_start
+    distance = pulse_duration * 17150
+    distance = round(distance, 2)
+    return distance
+print dis()
+#print "Distance:",distance,"cm"
 
-GPIO.output(TRIG, True)
-time.sleep(0.00001)
-GPIO.output(TRIG, False)
-print GPIO.input(ECHO)
-while GPIO.input(ECHO)==0:
-  pulse_start = time.time()
 
-while GPIO.input(ECHO)==1:
-  pulse_end = time.time()
-
-pulse_duration = pulse_end - pulse_start
-
-distance = pulse_duration * 17150
-
-distance = round(distance, 2)
-
-print "Distance:",distance,"cm"
-
-GPIO.cleanup()
+#GPIO.cleanup()
